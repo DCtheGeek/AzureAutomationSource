a0cc0f2fa393eac1f92aee9fd179f50924703d06
David Coulter <David.Coulter@microsoft.com>
2015-09-20 13:33:22 -0700

    Added Test-Webhook.ps1 example

---
+++ Sub1/Runbooks/Test-Webhook.ps1
@@ -0,0 +1,22 @@
+﻿workflow Test-Webhook
+{
+    param ( 
+        [object]$WebhookData
+    )
+
+    # If runbook was called from Webhook, WebhookData will not be null.
+    if ($WebhookData -ne $null) {   
+        # Collect properties of WebhookData
+        $WebhookName    =   $WebhookData.WebhookName
+        $WebhookHeaders =   $WebhookData.RequestHeader
+        $WebhookBody    =   $WebhookData.RequestBody
+
+        # Collect individual headers. VMList converted from JSON.
+        $From = $WebhookHeaders.From
+        $VMList = (ConvertFrom-Json -InputObject $WebhookBody).VirtualMachines
+        Write-Output "Runbook started from webhook $WebhookName by $From."
+    }
+    else {
+        Write-Error "Runbook meant to be started only from webhook." 
+    } 
+}
